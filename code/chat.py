7887b13a0fd775089eb52c0fd69507537424d040
Nipun Ganepola <85781012+Nipun44@users.noreply.github.com>
2023-10-24 22:05:13 +0530

    reduce unwanted

--- code/chat.py
+++ code/chat.py
@@ -28,8 +28,8 @@ classes = pickle.load(open('code/classes.pkl','rb'))
 
 # Connect to MongoDB
 client = MongoClient('mongodb+srv://Nipun:irwa@cluster0.wl2trfq.mongodb.net/')
-db = client['bookstore']
-books_collection = db['books']
+db = client['bookstore'] #db name
+books_collection = db['books'] #table name
 
 # Function to get the number of available books for a specific title
 def get_available_books(title):
@@ -116,204 +116,13 @@ def chatbot_response(text):
         
 
 
-user_query = "How many books available in 'To Kill a Mockingbird'"
+user_query = "How many books available in '1984'"
 response = chatbot_response(user_query)
 print(response)
+
 """GUI Interface
 
 """
-
-# import tkinter
-# from tkinter import *
-
-# BG_GRAY = "#ABB2B9"
-# BG_COLOR = "#1c172a"
-# TEXT_COLOR = "#000010"
-
-# # BG_GRAY = "#ABB2B9"
-# # BG_COLOR = "#1c172a"
-# # TEXT_COLOR = "#ffffff"
-
-
-# FONT = "Helvetica 14"
-# FONT_BOLD = "Helvetica 13 bold"
-
-
-# def send(event):
-#     msg = EntryBox.get("1.0",'end-1c').strip()
-#     EntryBox.delete("0.0",END)
-#     if msg != '':
-#         ChatLog.config(state=NORMAL)
-#         ChatLog.insert(END, "You: " + msg + '\n\n')
-#         ChatLog.config(foreground="#000000", font=("Verdana", 12 ))
-
-#         res = chatbot_response(msg)
-#         ChatLog.insert(END, "Bot: " + res + '\n\n')
-
-#         ChatLog.config(state=DISABLED)
-#         ChatLog.yview(END)
-
-
-# base = Tk()
-# base.title("E-Commerce Chatbot")
-# base.resizable(width=FALSE, height=FALSE)
-# base.configure(width=800, height=800, bg=BG_COLOR)
-
-
-# #Create Chat window
-# ChatLog = Text(base, bd=0, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT_BOLD)
-# ChatLog.config(state=DISABLED)
-
-# head_label = Label(base, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome to E-Commerce Chatbot", font=FONT_BOLD, pady=10)
-# head_label.place(relwidth=1)
-
-# line = Label(base, width=450, bg=BG_GRAY)
-
-
-# #Bind scrollbar to Chat window
-# scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-# ChatLog['yscrollcommand'] = scrollbar.set
-# ChatLog.focus()
-
-# #Create Button to send message
-# SendButton = Button(base, font=("Verdana", 12,'bold'), text="Send", width="12", height=15,
-#                     bd=0, bg="#ed9061", activebackground="#3c9d9b",fg='#ffffff',
-#                     command=lambda: send)
-
-# #Create the box to enter message
-# EntryBox = Text(base, bg="white",width="29", height="5", font="Arial", background="#dddddd")
-# EntryBox.focus()
-# EntryBox.bind("<Return>", send)
-# #EntryBox.bind("<Return>", send)
-
-
-
-
-
-# scrollbar.place(x=775,y=6, height=800)
-# line.place(x=0,y=35, height=1, width=770)
-# ChatLog.place(x=5,y=40, height=700, width=770)
-# EntryBox.place(x=0, y=740, height=60, width=600)
-# SendButton.place(x=600, y=740, height=60, width=175)
-
-#######################################best
-# import tkinter as tk
-
-# BG_GRAY = "#ABB2B9"
-# BG_COLOR = "#c5f0e3"
-# TEXT_COLOR = "#000000"
-
-# def send():
-#     msg = entry_box.get()
-#     entry_box.delete(0, tk.END)
-#     if msg.strip() != '':
-#         chat_log.config(state=tk.NORMAL)
-#         chat_log.insert(tk.END, "You: " + msg + '\n\n')
-#         chat_log.config(foreground="#000000", font=("Verdana", 12))
-
-#         res = chatbot_response(msg)  # Replace with your chatbot logic
-#         chat_log.insert(tk.END, "Bot: " + res + '\n\n')
-
-#         chat_log.config(state=tk.DISABLED)
-#         chat_log.yview(tk.END)
-
-# base = tk.Tk()
-# base.title("E-Commerce Chatbot")
-# base.geometry("800x800")
-
-# chat_log = tk.Text(base, bd=0, bg=BG_COLOR, fg=TEXT_COLOR, font=("Helvetica 13 bold"))
-# chat_log.config(state=tk.DISABLED)
-
-# head_label = tk.Label(base, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome to E-Commerce Chatbot", font=("Helvetica 13 bold"), pady=10)
-# head_label.pack(fill=tk.X)
-
-# entry_box = tk.Entry(base, bg="white", font=("Arial", 12))
-# entry_box.bind("<Return>", lambda event=None: send())
-# entry_box.pack(fill=tk.BOTH, padx=5, pady=5)
-
-# send_button = tk.Button(base, text="Send", command=send, font=("Verdana", 12, 'bold'), bg="#ed9061", activebackground="#3c9d9b", fg='#ffffff')
-# send_button.pack(fill=tk.BOTH)
-
-# scrollbar = tk.Scrollbar(base, command=chat_log.yview, cursor="heart")
-# chat_log['yscrollcommand'] = scrollbar.set
-# scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-# chat_log.pack(fill=tk.BOTH, expand=True)
-
-# base.mainloop()
-
-
-
-# import tkinter
-# from tkinter import *
-
-# BG_GRAY = "#ABB2B9"
-# BG_COLOR = "#c5f0e3"
-# TEXT_COLOR = "#000000"
-
-# # BG_GRAY = "#ABB2B9"
-# # BG_COLOR = "#1c172a"
-# # TEXT_COLOR = "#ffffff"
-
-
-# FONT = "Helvetica 14"
-# FONT_BOLD = "Helvetica 13 bold"
-
-
-# def send(event):
-#     msg = EntryBox.get("1.0",'end-1c').strip()
-#     EntryBox.delete("0.0",END)
-#     if msg != '':
-#         ChatLog.config(state=NORMAL)
-#         ChatLog.insert(END, "You: " + msg + '\n\n')
-#         ChatLog.config(foreground="#000000", font=("Verdana", 12 ))
-
-#         res = chatbot_response(msg)
-#         ChatLog.insert(END, "Bot: " + res + '\n\n')
-
-#         ChatLog.config(state=DISABLED)
-#         ChatLog.yview(END)
-
-
-# base = Tk()
-# base.title("E-Commerce Chatbot")
-# base.resizable(width=FALSE, height=FALSE)
-# base.configure(width=800, height=800, bg=BG_COLOR)
-
-
-# #Create Chat window
-# ChatLog = Text(base, bd=0, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT_BOLD)
-# ChatLog.config(state=DISABLED)
-
-# head_label = Label(base, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome to E-Commerce Chatbot", font=FONT_BOLD, pady=10)
-# head_label.place(relwidth=1)
-
-# line = Label(base, width=450, bg=BG_GRAY)
-
-
-# #Bind scrollbar to Chat window
-# scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-# ChatLog['yscrollcommand'] = scrollbar.set
-# ChatLog.focus()
-
-# #Create Button to send message
-# SendButton = Button(base, font=("Verdana", 12,'bold'), text="Send", width="12", height=15,
-#                     bd=0, bg="#ed9061", activebackground="#3c9d9b",fg='#ffffff',
-#                     command=lambda: send)
-
-
-# EntryBox = Text(base, bg="white",width="29", height="5", font="Arial", background="#dddddd")
-# EntryBox.focus()
-# EntryBox.bind("<Return>", send)
-
-
-# scrollbar.place(x=775,y=6, height=800)
-# line.place(x=0,y=35, height=1, width=770)
-# ChatLog.place(x=5,y=40, height=700, width=770)
-# EntryBox.place(x=0, y=740, height=60, width=600)
-# SendButton.place(x=600, y=740, height=60, width=175)
-
-# base.mainloop()
 
 
 
